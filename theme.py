1a14b46868dd87e3d31a34c034356de9a2eac58a
messense <messense@icloud.com>
2015-01-01 13:36:45 +0800

    Update theme and filters

---
+++ theme.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+name = 'lingonberry'
+author = 'Messense'
+homepage = 'https://github.com/messense/catsup-theme-lingonberry'
+post_per_page = 4
+
+vars = {
+    # whether to display excerpt or full article in post list
+    "display_thumbnail": True,
+    "use_excerpt": False,
+    "max_excerpt_length": 100,
+    "links": [
+        {
+            "name": "catsup",
+            "url": "https://github.com/whtsky/catsup",
+            "description": "Awesome!"
+        }
+    ]
+}
